0896a5fa72533f3670e5afb55a5de6719194577b
bernatsolat <149235839+bernatsolat@users.noreply.github.com>
2024-04-21 19:48:39 +0200

    sprint and background parallax

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class JohnMovement : MonoBehaviour
 {
     public float Speed;
-    
+    public float SprintSpeed;
     public GameObject BulletPrefab;
 
     private Rigidbody2D Rigidbody2D;
@@ -13,6 +13,7 @@ public class JohnMovement : MonoBehaviour
     private float Horizontal;
     private float LastShoot;
     private int Health = 5;
+    
 
     private void Start()
     {
@@ -28,8 +29,12 @@ public class JohnMovement : MonoBehaviour
         if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
         else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
-        Animator.SetBool("running", Horizontal != 0.0f);
 
+        
+        
+
+        Animator.SetBool("running", Horizontal != 0.0f);
+        
 
         // Detectar Suelo
         // Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
@@ -45,8 +50,14 @@ public class JohnMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Rigidbody2D.velocity = new Vector4(Horizontal * SprintSpeed, Rigidbody2D.velocity.y);
+        }
+        else { Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y); }
     }
+
+    
     public void Hit()
     {
         Health -= 1;
