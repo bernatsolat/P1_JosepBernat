1297bce7ccb014d37969b173d66d28d611cd018c
bernatsolat <149235839+bernatsolat@users.noreply.github.com>
2024-06-21 21:52:19 +0200

    update

--- Assets/Scripts/checkPlayerIn.cs
+++ Assets/Scripts/checkPlayerIn.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class checkPlayerIn : MonoBehaviour
 {
     private List<GameObject> cajas = new List<GameObject>();
-
+    private GameObject finalBoss;
+    private GameObject healthBoss;
     private void Start()
     {
+        GameObject Pause = GameObject.Find("Pause");
+        Transform healthBossTransform = Pause.transform.Find("HUD/BarraDeVida");
+        healthBoss = healthBossTransform.gameObject;
+        finalBoss = GameObject.Find("Jefe");
         cajas.Add(GameObject.Find("crate-stack (8)"));
         cajas.Add(GameObject.Find("crate-stack (9)"));
         cajas.Add(GameObject.Find("crate-stack (10)"));
         cajas.Add(GameObject.Find("crate-stack (11)"));
         cajas.Add(GameObject.Find("crate-stack (12)"));
         cajas.Add(GameObject.Find("crate-stack (13)"));
-
         foreach (GameObject caja in cajas)
         {
             caja.SetActive(false);
-        }
+        }        
+        finalBoss.SetActive(false);
+        healthBoss.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -28,6 +34,9 @@ public class checkPlayerIn : MonoBehaviour
             foreach (GameObject caja in cajas)
             {
                 caja.SetActive(true);
+                finalBoss.SetActive(true);
+                healthBoss.SetActive(true);
+
             }
         }
     }
